ab2eb5a4e5e9b7e5c41fd212834bc5ed28526e22
Sagarahir43 <120082667+Sagarahir43@users.noreply.github.com>
2023-07-19 09:08:40 +0530

    Add files via upload

---
+++ Assignment/Module 1/occurrences of each word in string.py
@@ -0,0 +1,9 @@
+str = input("Enter your string :")
+words = str.split()
+word_count = {}
+for word in words:
+    if word in word_count:
+        word_count[word]+=1
+    else:
+        word_count[word]=1
+print(word_count)           
