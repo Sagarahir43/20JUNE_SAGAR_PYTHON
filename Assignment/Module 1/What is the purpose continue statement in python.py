0837c46b3aa7f9c6cc49592b4e24164ba0d81db5
Sagarahir43 <120082667+Sagarahir43@users.noreply.github.com>
2023-07-19 09:16:49 +0530

    Add files via upload

---
+++ Assignment/Module 1/What is the purpose continue statement in python.py
@@ -0,0 +1,8 @@
+"""The continue keyword is used to end the current iteration in a for loop (or a while loop), 
+and continues to the next iteration."""
+
+
+for var in "I LOVE PYTHON":
+	if var == "E":
+		continue
+	print(var)
