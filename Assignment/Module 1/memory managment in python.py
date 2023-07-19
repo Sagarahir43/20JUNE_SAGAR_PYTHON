ca77b34a7b5ff735bf3c8096e348456104a7256d
Sagarahir43 <120082667+Sagarahir43@users.noreply.github.com>
2023-07-19 09:07:27 +0530

    Add files via upload

---
+++ Assignment/Module 1/memory managment in python.py
@@ -0,0 +1,7 @@
+"""Memory management in Python involves a private heap containing all Python objects and data structures. 
+The management of this private heap is ensured internally by the Python memory manager."""
+"""here we decalred a variable called 'a' and it stored a integer value it means now a's value is 10"""
+
+
+a = 10
+print("value of a is:",a)
