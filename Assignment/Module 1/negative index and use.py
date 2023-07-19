5a1f66f462734dd01745dfb3a773e927a807b76d
Sagarahir43 <120082667+Sagarahir43@users.noreply.github.com>
2023-07-19 09:07:57 +0530

    Add files via upload

---
+++ Assignment/Module 1/negative index and use.py
@@ -0,0 +1,8 @@
+"""In Python, negative indexes are used to access elements in a sequence (such as a string, list, or tuple)
+from the end of the sequence rather than from the beginning. Negative indexes count positions backward, 
+with -1 referring to the last element, -2 referring to the second-to-last element, and so on.
+"""
+#Here's an example to illustrate the use of negative indexes
+
+str = ("hello python")
+print(str[-3:])
