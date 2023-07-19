3996310b5cf57b9b830b8e6400aca21407305653
Sagarahir43 <120082667+Sagarahir43@users.noreply.github.com>
2023-07-19 09:13:47 +0530

    Add files via upload

---
+++ Assignment/Module 1/wap for int value same and their difference.py
@@ -0,0 +1,6 @@
+n1 = int(input("Enter the first number to check :"))
+n2 = int(input("Enter the second number to check :"))
+if (n1==n2 or n1 - n2 == 5 or n1 + n2 == 5):
+    print("True")
+else:
+    print("False")    
