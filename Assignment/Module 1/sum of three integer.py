39c7fd7c0ca29dacda708f9b4c15681e09c14dd9
Sagarahir43 <120082667+Sagarahir43@users.noreply.github.com>
2023-07-19 09:11:43 +0530

    Add files via upload

---
+++ Assignment/Module 1/sum of three integer.py
@@ -0,0 +1,11 @@
+a = int(input("Enter value of a :"))
+b = int(input("Enter value of b :"))
+c = int(input("Enter value of c :"))
+sum = 0
+if(a==b or a==c or c==b):
+    print("Sum is :",sum)
+else:
+    sum = a + b + c
+    print("Sum is :",sum)
+        
+    
