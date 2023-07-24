817fc18b8c89fed55ab019e3cc302ebb7f8acc36
Sagarahir43 <120082667+Sagarahir43@users.noreply.github.com>
2023-07-24 10:10:56 +0530

    Add files via upload

---
+++ Assignment/module 3/find the maximum and minimum number from the spicifird decimal number.py
@@ -0,0 +1,8 @@
+decimal_numbers={3.14,2.78,4.25,0.557,1.457}
+
+maximum=max(decimal_numbers)
+minimum=min(decimal_numbers)
+
+print("decimal number:",decimal_numbers)
+print("maximum number:",maximum)
+print("minimum number:",minimum)
