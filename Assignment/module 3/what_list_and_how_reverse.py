74cf0920c2561c3ccdd8a67d14274a673523c20b
Sagarahir43 <120082667+Sagarahir43@users.noreply.github.com>
2023-08-02 16:30:43 +0530

    Add files via upload

---
+++ Assignment/module 3/what_list_and_how_reverse.py
@@ -0,0 +1,7 @@
+"""a list is a built-in data structure that represents an ordered collection of elements. 
+It is mutable, which means you can modify its contents by adding, removing, or 
+changing items."""
+
+my_list  = ['apple','banana','mango','cherry']
+my_list.reverse()
+print(my_list)
