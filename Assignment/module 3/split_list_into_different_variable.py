b8bfc044859c5494d9948b9f3bbb0d436714d7e8
Sagarahir43 <120082667+Sagarahir43@users.noreply.github.com>
2023-08-02 16:49:55 +0530

    Add files via upload

---
+++ Assignment/module 3/split_list_into_different_variable.py
@@ -0,0 +1,8 @@
+#wap for split list into different variable
+list = ["python","java","c","c++","flutter","html","css","java script",]
+a = list[:4]
+b = list[4:]
+print("List in different variable :")
+print("a =",a)
+print("b =",b)
+
