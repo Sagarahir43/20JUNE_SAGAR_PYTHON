37770e39c13e1483660d790667233888cf5420ad
Sagarahir43 <120082667+Sagarahir43@users.noreply.github.com>
2023-08-02 16:31:51 +0530

    Add files via upload

---
+++ Assignment/module 3/check_presense_of_keys_in_dictionary.py
@@ -0,0 +1,6 @@
+dict = {"a":1,"b":2,"c":3,"d":4,"e":5}
+my_key = input("Enter your key :")
+if my_key in dict.keys():
+    print("key exists in dictionary ")
+else:
+    print("Key doesn't exists in dictionary ")    
