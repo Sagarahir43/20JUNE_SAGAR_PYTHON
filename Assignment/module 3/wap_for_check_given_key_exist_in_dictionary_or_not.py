d8d2e7541bd69a8649f26f49dd617c80d6f79c21
Sagarahir43 <120082667+Sagarahir43@users.noreply.github.com>
2023-08-02 16:44:28 +0530

    Add files via upload

---
+++ Assignment/module 3/wap_for_check_given_key_exist_in_dictionary_or_not.py
@@ -0,0 +1,6 @@
+dict1 = {"mango":4,"apple":5,"pinapple":7}
+my_key = input("enter key you want to check :")
+if my_key in dict1.keys():
+    print("Your key exits in dictionary")
+else:
+    print("your key not exits in dictionary")    
