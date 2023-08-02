f57e4bb41f31aa8911d422cd340c55732637ede8
Sagarahir43 <120082667+Sagarahir43@users.noreply.github.com>
2023-08-02 16:34:09 +0530

    Add files via upload

---
+++ Assignment/module 3/compare_two_list.py
@@ -0,0 +1,14 @@
+list1 = [20,58,78,56,54]
+list2 = [58,78,20,54,56]
+if len(list1) != len(list2):
+    print("List is not equal :")
+else:
+    sorted_list1  = sorted(list1)
+    sorted_list2  = sorted(list2)
+    if sorted_list1 == sorted_list2:
+        print("Both list are equal ")
+        
+
+
+
+    
