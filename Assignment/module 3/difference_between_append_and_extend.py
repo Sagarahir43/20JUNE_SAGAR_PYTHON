37396de3f6982b58d16b599a3dc58233892ec6c7
Sagarahir43 <120082667+Sagarahir43@users.noreply.github.com>
2023-08-02 16:38:45 +0530

    Add files via upload

---
+++ Assignment/module 3/difference_between_append_and_extend.py
@@ -0,0 +1,15 @@
+#append is add an object in last of the string. 
+list1 = ['python','java','c','c++']
+print("-------------------before append--------------------")
+print(list1)
+print("-------------------after append---------------------")
+list1.append('android')
+print(list1)
+#extend is add the new list in currunt list
+str = ['android','flutter','JC','Django']
+list1.extend(str)
+print(list1)
+
+
+
+
