78a78a53d38749e73768d85be8be3159fbdd8688
Sagarahir43 <120082667+Sagarahir43@users.noreply.github.com>
2023-08-02 16:47:28 +0530

    Add files via upload

---
+++ Assignment/module 3/square_1_to_30.py
@@ -0,0 +1,6 @@
+list_square = []
+for i in range(1,31):
+    x = i*i
+    list_square.append(x)
+first_5_elements = print(f"First 5 elements of list = {list_square[:5]}")
+last_5_elements = print(f"last five elements of list {list_square[-5:]}")    
